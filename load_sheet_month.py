d458036a79e5652c7d0b85ecfc9f772abe68ca6a
Christian Haederle <christian@haederle.org>
2023-12-27 13:36:37 +0100

    Successful reading of the Execel table

--- load_sheet_month.py
+++ load_sheet_month.py
@@ -1,6 +1,25 @@
 """Load worktimes from a Excel sheet"""
 
 import worktime
+from openpyxl import load_workbook
+
+def quick_hack(filename):
+    wb = load_workbook(filename=filename)
+    sheet = wb[wb.sheetnames[0]]
+    offset = 5
+
+    while sheet.cell(offset, 1).value:
+        line = [
+            sheet.cell(offset, 1).value,
+            sheet.cell(offset, 2).value,
+            sheet.cell(offset, 3).value,
+            sheet.cell(offset, 5).value,
+            sheet.cell(offset, 6).value,
+        ]
+        print(str(line))
+        offset += 1
+        if offset > sheet.max_row:
+            break
 
 class list(worktime.raw_list):
     """List of worktime entrys from .xlsx file"""
@@ -10,7 +29,7 @@ class list(worktime.raw_list):
         Arguments:
         file_ -- path/filename to the logfile
         """
-        pass
+        quick_hack(file_)
     def append(self, file_):
         """Appand entrys to the raw list
 
